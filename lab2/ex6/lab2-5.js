3ee2516412baac1498f7d549bf72528a5ff9f3d1
emxin300 <tung4m@gmail.com>
2017-02-12 12:50:39 +0200

    adding ex6 of lab2 and ex2 of lab4

---
+++ lab2/ex6/lab2-5.js
@@ -0,0 +1,7 @@
+   
+
+function add() {
+	 var x =document.getElementById("text").value;
+	var y =document.getElementById("names").innerHTML;
+	document.getElementById("names").innerHTML = y  + x + "<br/>";
+}
